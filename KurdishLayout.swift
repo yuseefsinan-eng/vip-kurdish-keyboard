66fd8fbf3b8b5ee4337f5d3cc92c79e28285ab31
yuseefsinan-eng <yuseefsinan@gmail.com>
2026-09-08 06:54:06 +0300

    Create KurdishLayout.swift

---
+++ KurdishLayout.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct KurdishLayout {
+    // ڕێزبەندیا پیتێن کوردی (بادینی و سۆرانی) بۆ کیبۆردا VIP
+    static let rowFirst: [String] = ["ق", "و", "ە", "ر", "ت", "ي", "و", "پ", "ۆ", "چ"]
+    static let rowSecond: [String] = ["ا", "س", "د", "ف", "گ", "ه", "ج", "ک", "ل", "ێ"]
+    static let rowThird: [String] = ["ز", "ش", "ع", "ب", "ن", "م", "ڤ", "ژ"]
+    
+    static let englishRowFirst: [String] = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+    static let englishRowSecond: [String] = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+    static let englishRowThird: [String] = ["z", "x", "c", "v", "b", "n", "m"]
+}
